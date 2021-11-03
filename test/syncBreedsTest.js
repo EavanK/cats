691ddd8e36acc7e0d13d9332c8f2df2619a2cb58
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-03 05:25:45 +0000

    Testing Asynchronous Return Values

---
+++ test/syncBreedsTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+
+    console.log(breedDetails);
+  });
+
+  it('returns breed details for the Balinese breed', () => {
+
+    console.log(breedDetails);
+  });
+});
